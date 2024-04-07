ca4b7780dc271618f1f0a45979671a9a3bd6ca05
christopher <christophervos@gmail.com>
2024-04-07 16:27:51 +0200

    Created button component

---
+++ src/lib/components/Button.svelte
@@ -0,0 +1,28 @@
+<button class="btn" on:click>
+  <slot></slot>
+</button>
+
+<style>
+  .btn {
+    font-size: 1rem;
+
+    margin: .25em 0;
+    padding: .4em;
+
+    color: var(--primary-200);
+
+    border: 3px solid var(--primary-200);
+    border-radius: .35em;
+    background-color: var(--accent-100);
+
+    transition: color var(--duration) var(--zip);
+  }
+  .btn:hover {
+    color: var(--bg-200);
+
+    background-color: var(--primary-200);
+  }
+  .btn:active {
+    transform: scale(0.97);
+  }
+</style>
