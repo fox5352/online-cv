c0082302e87944a8c9f41b2c06413913e673eed0
christopher <christophervos522@gmail.com>
2024-12-27 21:40:27 +0200

    finsihed about section

--- src/pages/Home/Page.tsx
+++ src/pages/Home/Page.tsx
@@ -1,17 +1,54 @@
+import { motion } from "motion/react";
+import LetterHop from "../../ui/animated/LetterHop";
 import styles from "./Page.module.css";
-
 import me from "../../assets/me.webp";
+
+const DT_DURATION = 1;
+const DT_DELAY = 0.5;
+const NAME = "Christopher";
+const ROLE = "Software Developer";
 
 function Home() {
   return (
     <main className={styles.main}>
-      <section className={styles.about} data-section="about">
+      <motion.section
+        className={styles.about}
+        data-section="about"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: DT_DURATION / 3 }}
+      >
         <div className={styles.contentBox}>
           <h2>
-            Hi, I'm <span>Christopher</span>
+            Hi, I
+            <motion.span
+              initial={{
+                color: "inherit",
+              }}
+              animate={{ color: "var(--ac-two)" }}
+              transition={{
+                delay: DT_DELAY - 0.1,
+                duration: DT_DURATION,
+              }}
+            >
+              '
+            </motion.span>
+            m{" "}
+            <LetterHop
+              data={NAME.split("")}
+              delay={DT_DELAY}
+              color="var(--ac-two)"
+              duration={DT_DURATION}
+            />
           </h2>
           <h4>
-            Graduate <span>Software Developer</span>
+            Graduate{" "}
+            <LetterHop
+              data={ROLE.split("")}
+              delay={DT_DELAY}
+              color="var(--ac-one)"
+              duration={DT_DURATION}
+            />
           </h4>
           <p>
             A problem solver and a quick learner, always eager to face
@@ -19,14 +56,20 @@ function Home() {
             built with Svelte, showcasing my solid foundation in UX design
             principles. I possess a demonstrated understanding of best coding
             practices and am enthusiastic about contributing to the creation of
-            dynamic and responsive websites
+            dynamic and responsive websites..
           </p>
         </div>
         {/* -------------------------------------------------------------------------------- */}
-        <div className={styles.imageBox}>
-          <img src={me} alt="an image of me the devloper" />
-        </div>
-      </section>
+        <motion.div className={styles.imageBox}>
+          <motion.img
+            src={me}
+            alt="an image of me the developer"
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ delay: DT_DELAY, duration: DT_DURATION }}
+          />
+        </motion.div>
+      </motion.section>
     </main>
   );
 }
